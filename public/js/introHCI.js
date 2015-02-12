67eb73ac87f1f7296cd0fe6975923a97685afad0
James Keophavone <jame_zie@yahoo.com>
2015-02-12 15:43:25 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,16 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+  $.get("/project/"+idNumber, function (result) {
+    console.log("Making AJAX call to: /project/"+idNumber);
+    var addProjectString = "<img src='"+result.image+"' class='detailsImage'>"+
+                           "<h3>Title: "+result.title+"</h3>"+
+                           "<h3>Date: "+result.date+"</h3>"+
+                           "<h3>Summary:</h3><br>"+result.summary;
+
+    $("#project"+idNumber+" div.details").html(addProjectString);
+  });
 }
 
 /*
@@ -35,4 +45,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+  $.get("/palette", function(result) {
+    var colors = result.colors.hex;
+
+    $('body').css('background-color', colors[0]);
+    $('thumbnail').css('background-color', colors[1]);
+    $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+    $('p').css('color', colors[3]);
+    $('.project img').css('opacity', .75);
+
+  });
+
 }
